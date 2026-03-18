20c2c2483932a4b238af892b91373d8a924345d7
lipebol <lipe_bol5@hotmail.com>
2026-03-18 00:27:41 -0300

    multi changes and add 'alert'

--- common/notific.py
+++ common/notific.py
@@ -1,7 +1,7 @@
-from .authEx import auth
-from .loadEx import load, system
-from .mountEx import mount
-from inspect import currentframe
+from .authentic import Auth
+from .fabric import Make
+from .utilize import Use
+from subprocess import run, PIPE
 from traceback import format_exc
 
 
@@ -14,33 +14,40 @@ class Notific:
                 return func(*args, **kwargs)
             except Exception as error:
                 try:
-                    if (widgEx := load.widgex(currentframe().f_back)):
-                        system.notifysend(
-                            title=widgEx, message=notific.event(
-                                summary=f"{widgEx} ({type(error).__name__})", 
-                                description=format_exc(), colorId='11'
-                            ).get('kind')
-                        )
+                    Notific.alert(
+                        title='Notific', message=Notific.event(
+                            summary=f"Notific ({type(error).__name__})", 
+                            description=format_exc(), colorId='11'
+                        ).get('kind')
+                    )
                 except Exception as error:
-                    system.notifysend(title=widgEx, message=format_exc())
+                    Notific.alert(title='Notific', message=format_exc())
         return wrapper
     
     @staticmethod
     def event(
         *, summary: str, description: str, colorId: str, 
-        date = mount.data(dateTime=load.now())
+        date = Make.data(dateTime=Use.now())
     ):
-        return auth.googleservice(
-            name='calendar', version='v3', 
-            _auth=load.variable('NOTIFIC_AUTH'), 
-            _scope=load.variable('NOTIFIC_SCOPE')
+        return Auth.gcp_service(
+            name='calendar', version='v3', _auth=Use.variable('NOTIFIC_AUTH'), 
+            _scope=Use.variable('NOTIFIC_SCOPE')
         ).events().insert(
-            calendarId=system.decr(variable=load.variable('NOTIFIC_ID')), 
-            body=mount.data(
+            calendarId=Use.decr(variable=Use.variable('NOTIFIC_ID')), 
+            body=Make.data(
                 summary=summary, description=description, 
                 colorId=colorId, start=date, end=date
             )
         ).execute()
+
+    @staticmethod
+    def alert(*, title: str = 'WARNING!', message: str):
+        if (load_alert := Use.variable('NOTIFIC_SEND')):
+            return run(
+                load_alert % (f'''"👉 {title} 👈\n\n""{message}"'''),
+                shell=True, stdout=PIPE, text=True
+            ).stdout.strip()
+        raise Exception(load_alert)
 
 
     
