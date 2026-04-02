f7801ae4530d072ee04f21f02bb7b0f7f5e01f24
lipebol <lipe_bol5@hotmail.com>
2026-04-02 13:20:50 -0300

    add 'get query'

--- spotifEx/v2/__init__.py
+++ spotifEx/v2/__init__.py
@@ -1,10 +1,18 @@
 from common.storage import Stor
 from common.utilize import Use
-from spotifEx.spotify_for_developers import WebAPI
+from fsspec.implementations.http import HTTPFileSystem
 
 Use.variable('DATARIZED_CORE_NAME', add='spotifEx')
 Use.variable('DATARIZED_CORE_VERSION', add='v2')
-setattr(Use, 'spotify_web_api', WebAPI.get_query('spotify-web-api'))
+
+with HTTPFileSystem().open(
+    (
+        'https://raw.githubusercontent.com/lipebol/datarized-core/'
+        'refs/heads/main/spotifEx/querys/spotify-web-api'
+    ), 'rb'
+) as content:
+    setattr(Use, 'spotify_web_api', content.read().decode())
+
 setattr(Use, 'start_date', Use.now(all=False))
 setattr(Use, 'log_id', set())
 
