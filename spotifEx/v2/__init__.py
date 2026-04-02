41e89f8549912218dddde60d8825721423a555cf
lipebol <lipe_bol5@hotmail.com>
2026-04-02 01:14:12 -0300

    up lines

--- spotifEx/v2/__init__.py
+++ spotifEx/v2/__init__.py
@@ -2,16 +2,15 @@ from common.storage import Stor
 from common.utilize import Use
 from spotifEx.spotify_for_developers import WebAPI
 
-Use.variable('SPOTIFY_WEB_API_QUERY', add=WebAPI.get_query('spotify-web-api'))
 Use.variable('DATARIZED_CORE_NAME', add='spotifEx')
 Use.variable('DATARIZED_CORE_VERSION', add='v2')
-
-Stor.setconfig()
+setattr(Use, 'spotify_web_api', WebAPI.get_query('spotify-web-api'))
+setattr(Use, 'start_date', Use.now(all=False))
+setattr(Use, 'log_id', set())
 
 if Use.checkpath((log_id := Use.walfile(id=True))):
-    with open(Use.walfile(id=True), 'r') as file:
-        setattr(Use, 'log_id', set(line.strip() for line in file))
-else:
-    setattr(Use, 'log_id', set())
+    with open(log_id, 'r') as walfile:
+        for id in walfile:
+            Use.log_id.add(id.strip())
 
-Use.info(Use.log_id)
+Stor.setconfig()
