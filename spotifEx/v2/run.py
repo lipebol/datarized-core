f6fc70ef7e0db834798e6e22cf20f02250a21bde
lipebol <lipe_bol5@hotmail.com>
2026-04-01 23:56:19 -0300

    add 'lock' and new method 'add_log_id', changed 'endless wait'

--- spotifEx/v2/run.py
+++ spotifEx/v2/run.py
@@ -1,15 +1,35 @@
-from asyncio import AbstractEventLoop, get_running_loop, run, run_coroutine_threadsafe
+from asyncio import AbstractEventLoop, Event, get_running_loop, run, run_coroutine_threadsafe
+from common.fabric import Make
 from common.notific import Notific
 from common.utilize import Use
+from common.storage import Stor
 from concurrent.futures import Future
 from dbus import Array, Dictionary, SessionBus, String
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 from spotifEx.spotify_for_developers import WebAPI
-from threading import Thread
+from threading import Lock, Thread
 
 
 class init:
+
+    _lock = Lock()
+
+    @staticmethod
+    def add_log_id(trackid: str):
+        with init._lock:
+            if trackid not in Use.log_id:
+                Use.log_id.add(trackid)
+                with open(Use.walfile(id=True), 'a') as file:
+                    file.write(trackid + '\n')
+                Use.info(Use.log_id)
+                return True
+
+    @staticmethod
+    def track(response: Future):
+        if (error := (track := response.result()).get('error')):
+            Use.info(error)
+        Use.jsonific(data=track, jsonl=True)
  
     @staticmethod
     def metadata(event_loop: AbstractEventLoop):
@@ -27,9 +47,12 @@ class init:
                 ):
                     raise Exception('There was probably an error connecting to DBUS.')
                 if '/com/spotify/ad/' not in (trackid := metadata.get('mpris:trackid')):
-                    run_coroutine_threadsafe(
-                        WebAPI.access(trackid), event_loop
-                    ).add_done_callback(init.spotifEx)
+                    if init.add_log_id((trackid := trackid.split('/')[-1])):
+                        run_coroutine_threadsafe(
+                            WebAPI.access(trackid), event_loop
+                        ).add_done_callback(init.track)
+                    else:
+                        Use.jsonific(data={'trackid': trackid}, jsonl=True)
         try:
             DBusGMainLoop(set_as_default=True)
             SessionBus().add_signal_receiver(
@@ -42,17 +65,8 @@ class init:
         except Exception as error:
             Use.info(error)
 
-    @staticmethod
-    def spotifEx(track: Future):
-        if not (error := (track := track.result()).get('error')):
-            print(track)
-            return 
-            # if not track.get('id'):
-            #     track['artists'] = list(init.artists(track.get('artists')))
-            #     track['album'] = init.album(track.get('album'))
-            #     track = str(MongoDB.insert('tracks', data=track))
-            # return init.daylist(track)
-        Use.info(error)
+    # print(track)
+    # print(Table.from_pylist([track], schema=Make.schema('Track_v2')))
 
     @staticmethod
     async def run():
@@ -61,7 +75,7 @@ class init:
             args=((event_loop := get_running_loop()),)
         ).start()
 
-        await event_loop.create_future()
+        await Event().wait()
 
 
 if __name__ == '__main__':
