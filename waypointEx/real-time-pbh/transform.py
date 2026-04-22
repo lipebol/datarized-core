152e85d32fea02e93ff46d56a276d8cea9bf28a6
lipebol <lipe_bol5@hotmail.com>
2026-04-22 11:04:18 -0300

    up lines

--- waypointEx/real-time-pbh/transform.py
+++ waypointEx/real-time-pbh/transform.py
@@ -6,21 +6,17 @@ import pyarrow.feather as feather
 csv_file = Use.read_csv(
     Use.path(
         (dir_files := Use.path(Use.this_path, join=['files'])), 
-        join='sup-bus-routes-conversion.csv'
+        join='bus-routes.csv'
     ), 
-    ['id', 'bus_id', 'bus_line', 'bus_route'], {
-        'id': uint16(), 'bus_id': string(), 
-        'bus_line': string(), 'bus_route': string()
+    ['bus_id', 'bus_line', 'bus_route'], {
+        'bus_id': string(), 'bus_line': string(), 'bus_route': string()
     }
-).drop('id')
-
-feather.write_feather(
-    csv_file, (
-        arrow_path := Use.path(
-            dir_files, join='sup-bus-routes-conversion.arrow'
-        )
-    ), compression='uncompressed'
 )
 
+feather.write_feather(
+    csv_file, (arrow_path := Use.path(dir_files, join='bus-routes.arrow')),
+    compression='uncompressed'
+)
 
-print(feather.read_table(arrow_path).num_rows)
+print((file := feather.read_table(arrow_path)))
+print(file.num_rows)
